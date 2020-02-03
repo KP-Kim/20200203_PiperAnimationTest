7e9e01ab33fafee8cfe33d1eace0434bb07f6ccc
KP-Kim <57761471+KP-Kim@users.noreply.github.com>
2020-02-03 15:45:23 -0500

    tried JS but wasnt working

--- 03_wave.js
+++ 03_wave.js
@@ -1,3 +1,16 @@
-feet.onclick = function() {
-    blue.style.animationPlayState = 'running';
+var currentLetter = 0;
+var intervalID;
+var clickCount = 0; 
+console.log = clickCount
+waves.onclick = function () {
+    if (clickCount == 1) purple.style.animationPlayState = 'running'; {
+    }
+    if (clickCount == 2) emerald.style.animationPlayState = 'running';{
+    }
+    if (clickCount == 3) blue.style.animationPlayState = 'running';{
+    }
 }
+
+// waves.onclick = function() {
+//     purple.style.animationPlayState = 'running';
+// }
