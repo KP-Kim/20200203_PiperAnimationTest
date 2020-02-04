079901a7e1ba6394e7b28a4c9d6df153349ded5a
KP-Kim <57761471+KP-Kim@users.noreply.github.com>
2020-02-04 12:51:57 -0500

    javascript succeeded but not final

--- 03_wave.js
+++ 03_wave.js
@@ -63,18 +63,18 @@ function onClick() {
     switch (iterations) {
         case 1:
             console.log ("wave_stage_1");
-            var transition = document.getElementById("blue");
-            transition.setAttribute("style", "transform: rotate(90deg); transition-duration: 3s");
+            var transition = document.getElementById("waves");
+            transition.setAttribute("style", "transform: translateY(-100px); transition-duration: 3s");
             break;
         case 2:
             console.log ("wave_state_2");
-            var transition = document.getElementById("feet");
-            transition.setAttribute("style", "transform: rotate(45deg)");
+            var transition = document.getElementById("waves");
+            transition.setAttribute("style", "transform: translateY(-200px); transition-duration: 3s");
             break;
         case 0:
             console.log ("write_stage_0");
-            var transition = document.getElementById("feet");
-            transition.setAttribute("style", "transform: rotate(0deg)");
+            var transition = document.getElementById("waves");
+            transition.setAttribute("style", "transform: translateY(0px); transition-duration: 3s");
             break;
         }
 
